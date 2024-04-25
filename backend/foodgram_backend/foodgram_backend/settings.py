4be28902ba6956527e54f0628c24f9c8f7585431
Владислав <VladislavVyt@yandex.ru>
2024-04-25 13:00:36 +0300

    Ver 1.3.1

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -95,6 +95,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 AUTH_USER_MODEL = 'alone_app.FoodgramUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
